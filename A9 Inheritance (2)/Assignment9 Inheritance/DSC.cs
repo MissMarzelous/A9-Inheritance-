09b593931de479b285db909030c4f32a332bf0ea
Marlena Fabrick <116241357+MissMarzelous@users.noreply.github.com>
2026-05-26 19:12:36 -0400

    Add files via upload
    Program Overview-
    This is an object-oriented programming example written in C# (.NET Core 3.1) that demonstrates class inheritance and method overriding. It models a college campus system where a Campus class inherits from a DSC (Daytona State College) parent class.
    
    Architecture & Class Hierarchy:
    
    Parent Class: DSC-
    Represents the main school entity
    Contains school name property
    Defines virtual method giveAddress() that can be overridden
    
    Child Class: Campus-
    Inherits from DSC
    Represents a specific campus location
    Overrides giveAddress() method with campus-specific address
    Implements ToString() method for object string representation
    Adds campus-specific properties and methods
    
    Functionality-
    The program creates a Campus object representing "Advanced Technology College"
    Calls ToString() which combines inherited and overridden methods
    Displays campus information including school name, campus name, address, and departments
    Demonstrates polymorphism through method overriding
    Technical Stack
    Language: C#
    Framework: .NET Core 3.1
    Paradigm: Object-Oriented Programming (OOP)
    Key Concepts: Inheritance, Virtual Methods, Method Overriding, ToString Override
    Project Type: Console Application

---
+++ A9 Inheritance (2)/Assignment9 Inheritance/DSC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment9_Inheritance
+{
+    class DSC
+    {
+        public virtual string giveAddress()
+        {
+            return "1200 W. International Speedway Blvd., Daytona Beach, Florida 32114";
+        }
+
+        public void set_schoolName(string sName)
+        { nameOfSchool = sName; }
+        private string nameOfSchool = "Daytona State College";
+        public string TheSchoolNme()
+        {
+            return nameOfSchool;
+          
+        }
+    }
+
+}
---
+++ A9 Inheritance (2)/Assignment9 Inheritance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Assignment9_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Campus atc = new Campus("Advanced Technology College");
+            Console.WriteLine(atc.ToString());
+            Console.Read();
+           
+
+                /// END PROGRAM
+        }
+    }
+}
